9b6311227df45d3c0d1663979831f671b48a49e2
rafagf <rafagarfer@gmail.com>
2021-02-06 22:04:20 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/d8658484ba578665ec3c4b06ac0f1b0e5b912582

---
+++ build/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"Well. Finally got around to putting this site up where I’ll share random thoughts and learnings in broken English. Glad to have you!  ","categories": [],
+        "tags": [],
+        "url": "https://rafagf.github.io/rafa-garcia-site-launched/",
+        "teaser": null
+      }]
