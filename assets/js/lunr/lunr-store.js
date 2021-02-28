196bfa38476f47a265dd53148e1da139f1f5d126
rafagf <rafagarfer@gmail.com>
2021-02-28 18:25:03 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/80612184930621386878de34b01163cdf862ad79

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Building my site using GitHub and Jekyll",
         "excerpt":"For 2021 I decided it was time to start sharing some of my developer adventures. I want to do this with the hope that it helps someone and as a way to learn for myself, as writing makes you aware of the things you know and the things you don’t....","categories": ["development"],
         "tags": ["web","jekyll","github"],
-        "url": "https://rafagf.github.io/development/building-my-site-using-github/",
+        "url": "https://rafagf.github.io/development/building-my-site-using-github-and-jekyll/",
         "teaser": null
       }]
