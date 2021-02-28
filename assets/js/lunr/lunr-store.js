17926d0552c495cbe53f2bd9912c19f9351e119a
rafagf <rafagarfer@gmail.com>
2021-02-28 11:32:30 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/2baa9581b507519de60d428b5d68d60913c12b3c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Welcome!",
-        "excerpt":"Well. Finally got around to putting this site up where I’ll share random thoughts and learnings in broken English. Glad to have you!  ","categories": [],
-        "tags": [],
-        "url": "https://rafagf.github.io/rafa-garcia-site-launched/",
+        "title": "Building my site using GitHub and Jekyll",
+        "excerpt":"For 2021 I decided it was time to start sharing some of my developer adventures. I want to do this with the hope that it helps someone and as a way to learn for myself, as writing makes you aware of the things you know and the things you don’t....","categories": ["development"],
+        "tags": ["web","jekyll","github"],
+        "url": "https://rafagf.github.io/development/building-my-site-using-github/",
         "teaser": null
       }]
