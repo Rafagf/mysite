14c8f040b64304b81a6b1261136ca884ce20311e
rafagf <rafagarfer@gmail.com>
2021-03-07 19:53:39 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/7ec4e87dd9fb9c30805cc3c55b5fe2c64aa7ff74

--- build/assets/js/lunr/lunr-store.js
+++ build/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["web","jekyll","github"],
         "url": "https://rafagf.github.io/development/building-my-site-using-github-and-jekyll/",
         "teaser": null
+      },{
+        "title": "Building a backend, an API and a web client using Ktor",
+        "excerpt":"Some time ago I had a potential client that wanted the following: An Android app that would send data to a backend. The app would also consume and display data from the backend. The consumed data would be created by an administrator in a website (dashboard look-alike). Pretty standard, right?...","categories": ["development"],
+        "tags": ["ktor","kotlin"],
+        "url": "https://rafagf.github.io/development/building-a-backend-an-api-and-a-web-client-using-ktor/",
+        "teaser": null
       }]
