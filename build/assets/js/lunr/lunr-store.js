2f84a3a33ab6ccd9cedb2d89f8b8ef0370a4764a
rafagf <rafagarfer@gmail.com>
2023-03-19 21:08:16 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/2609eb5d928c7e6055cba8d51770dda329be54e7

--- build/assets/js/lunr/lunr-store.js
+++ build/assets/js/lunr/lunr-store.js
@@ -2,42 +2,42 @@ var store = [{
         "title": "Building my site using GitHub and Jekyll",
         "excerpt":"For 2021 I decided it was time to start sharing some of my developer adventures. I want to do this with the hope that it helps someone and as a way to learn for myself, as writing makes you aware of the things you know and the things you don’t....","categories": ["development"],
         "tags": ["web","jekyll","github"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/building-my-site-using-github-and-jekyll/",
+        "url": "rafagarcia.dev/development/building-my-site-using-github-and-jekyll/",
         "teaser": null
       },{
         "title": "Building a backend, an API and a web client using Ktor",
         "excerpt":"Some time ago I had a potential client that wanted the following: An Android app that would send data to a backend. The app would also consume and display data from the backend. The consumed data would be created by an administrator in a website (dashboard look-alike). Pretty standard, right?...","categories": ["development"],
         "tags": ["ktor","kotlin"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/building-a-backend-an-api-and-a-web-client-using-ktor/",
+        "url": "rafagarcia.dev/development/building-a-backend-an-api-and-a-web-client-using-ktor/",
         "teaser": null
       },{
         "title": "Tracking code coverage for free",
         "excerpt":"In the Android team at Memrise, we’ve gotten a lot of value from tracking code coverage. It’s something we’ve worked very hard to improve over the last few years. The main two reasons being: Tests are great. They help us move faster and safer! Coverage is the indicator of how...","categories": ["development"],
         "tags": ["testing","android","mobile development"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/tracking-code-coverage-for-free/",
+        "url": "rafagarcia.dev/development/tracking-code-coverage-for-free/",
         "teaser": null
       },{
         "title": "Modelling the domain layer using composable use cases",
         "excerpt":"Some time ago we had a problem inside the Memrise Android codebase with our domain layer. Repositories and Use Cases were heavily used but there weren’t clear guidelines on what they were supposed to do and not do. It was common to find them being used in combination, which in...","categories": ["development"],
         "tags": ["android","mobile development","architecture"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/modelling-the-domain-layer-using-composables-use-cases/",
+        "url": "rafagarcia.dev/development/modelling-the-domain-layer-using-composables-use-cases/",
         "teaser": null
       },{
         "title": "Decompiling Kotlin into Java - Few years late",
         "excerpt":"Recently I was asked by a co-worker “what are sealed classes?” and realised the classic answer “enums in steroid” falls really short and a clear sign that I didn’t fully understand what they are. Because of this, I decided to spend some time decompiling Kotlin code into Bytecode and then...","categories": ["development"],
         "tags": ["android","mobile development","kotlin"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/decompiling-kotlin-into-java/",
+        "url": "rafagarcia.dev/development/decompiling-kotlin-into-java/",
         "teaser": null
       },{
         "title": "Architecting mobile apps with Kotlin Multiplatform",
         "excerpt":"Note: This article focuses on Kotlin Multiplatform Mobile (KMM), which can be seen as a subset of Kotlin Multiplatform (KMP). KMM is a hot topic in mobile development. Its main advantages can be summarised as: Sharing code across platforms: meaning less code to maintain, fewer bugs (or at least consistent...","categories": ["development"],
         "tags": ["android","iOS","mobile development","kotlin multiplatform","kotlin"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/architecting-mobile-apps-with-kotlin-multiplatform/",
+        "url": "rafagarcia.dev/development/architecting-mobile-apps-with-kotlin-multiplatform/",
         "teaser": null
       },{
         "title": "Automating releases with a Mobile Release Pipeline",
         "excerpt":"At Memrise we like our apps to be updated as often as possible, ideally in a weekly cadence. We’ve noticed this gives us numerous advantages versus for example releasing only when big features are ready: We can fix non-critical bugs as we go, without the need for hot-fixing. We can...","categories": ["development"],
         "tags": ["typescript","mobile development"],
-        "url": "https://rafagf.github.io/mysite/mysite/development/mobile-release-pipeline/",
+        "url": "rafagarcia.dev/development/mobile-release-pipeline/",
         "teaser": null
       }]
