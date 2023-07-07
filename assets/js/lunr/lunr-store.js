2699f6241554234da5ceb8cb30f1728036848fa9
rafagf <rafagarfer@gmail.com>
2023-07-07 18:52:36 +0000

    Update from https://github.com/Rafagf/rafagf.github.io-private/commit/7e837f3014ba47992e4c0b8ebc161bbad3d0f469

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,36 +1,36 @@
 var store = [{
         "title": "Building my site using GitHub and Jekyll",
-        "excerpt":"For 2021 I decided it was time to start sharing some of my developer adventures. I want to do this with the hope that it helps someone and as a way to learn for myself, as writing makes you aware of the things you know and the things you don’t....","categories": ["development"],
+        "excerpt":"For 2021 I decided it was time to start sharing some of my developer adventures. I want to do this with the hope that it helps someone and as a way to learn for myself, as writing makes you aware of the things you know and the things you don’t....","categories": [],
         "tags": ["web","jekyll","github"],
         "url": "/building-my-site-using-github-and-jekyll/",
         "teaser": null
       },{
         "title": "Building a backend, an API and a web client using Ktor",
-        "excerpt":"Some time ago I had a potential client that wanted the following: An Android app that would send data to a backend. The app would also consume and display data from the backend. The consumed data would be created by an administrator in a website (dashboard look-alike). Pretty standard, right?...","categories": ["development"],
+        "excerpt":"Some time ago I had a potential client that wanted the following: An Android app that would send data to a backend. The app would also consume and display data from the backend. The consumed data would be created by an administrator in a website (dashboard look-alike). Pretty standard, right?...","categories": [],
         "tags": ["ktor","kotlin"],
         "url": "/building-a-backend-an-api-and-a-web-client-using-ktor/",
         "teaser": null
       },{
         "title": "Tracking code coverage for free",
-        "excerpt":"In the Android team at Memrise, we’ve gotten a lot of value from tracking code coverage. It’s something we’ve worked very hard to improve over the last few years. The main two reasons being: Tests are great. They help us move faster and safer! Coverage is the indicator of how...","categories": ["development"],
+        "excerpt":"In the Android team at Memrise, we’ve gotten a lot of value from tracking code coverage. It’s something we’ve worked very hard to improve over the last few years. The main two reasons being: Tests are great. They help us move faster and safer! Coverage is the indicator of how...","categories": [],
         "tags": ["testing","android","mobile development"],
         "url": "/tracking-code-coverage-for-free/",
         "teaser": null
       },{
         "title": "Modelling the domain layer using composable use cases",
-        "excerpt":"Some time ago we had a problem inside the Memrise Android codebase with our domain layer. Repositories and Use Cases were heavily used but there weren’t clear guidelines on what they were supposed to do and not do. It was common to find them being used in combination, which in...","categories": ["development"],
+        "excerpt":"Some time ago we had a problem inside the Memrise Android codebase with our domain layer. Repositories and Use Cases were heavily used but there weren’t clear guidelines on what they were supposed to do and not do. It was common to find them being used in combination, which in...","categories": [],
         "tags": ["android","mobile development","architecture"],
         "url": "/modelling-the-domain-layer-using-composables-use-cases/",
         "teaser": null
       },{
         "title": "Decompiling Kotlin into Java - Few years late",
-        "excerpt":"Recently I was asked by a co-worker “what are sealed classes?” and realised the classic answer “enums in steroid” falls really short and a clear sign that I didn’t fully understand what they are. Because of this, I decided to spend some time decompiling Kotlin code into Bytecode and then...","categories": ["development"],
+        "excerpt":"Recently I was asked by a co-worker “what are sealed classes?” and realised the classic answer “enums in steroid” falls really short and a clear sign that I didn’t fully understand what they are. Because of this, I decided to spend some time decompiling Kotlin code into Bytecode and then...","categories": [],
         "tags": ["android","mobile development","kotlin"],
         "url": "/decompiling-kotlin-into-java/",
         "teaser": null
       },{
         "title": "Architecting mobile apps with Kotlin Multiplatform",
-        "excerpt":"Note: This article has been featured in both Android Weekly (#490) and Kotlin Weeekly (#273). Thanks to both! 🙏 KMM is a hot topic in mobile development. Its main advantages can be summarised as: Sharing code across platforms: meaning less code to maintain, fewer bugs (or at least consistent bugs...","categories": ["development"],
+        "excerpt":"Note: This article has been featured in both Android Weekly (#490) and Kotlin Weeekly (#273). Thanks to both! 🙏 KMM is a hot topic in mobile development. Its main advantages can be summarised as: Sharing code across platforms: meaning less code to maintain, fewer bugs (or at least consistent bugs...","categories": [],
         "tags": ["android","iOS","mobile development","kotlin multiplatform","kotlin"],
         "url": "/architecting-mobile-apps-with-kotlin-multiplatform/",
         "teaser": null
